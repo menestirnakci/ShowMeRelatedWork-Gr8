bea7dcb1b3da8a7793d68f7253f457a0dce5b12b
Muhammed Enes Tırnakçı <menestirnakci@gmail.com>
2021-01-18 12:39:55 +0300

    Update UrlSearch.py

--- UrlSearch.py
+++ UrlSearch.py
@@ -40,8 +40,6 @@ class UrlSearch(object):
             self.url = url
         
         self.request_page = requests.get(self.url)
-        if self.request_page.status_code != 200:
-            raise InvalidUrlError('Invalid Url')
         self.request_page_text = self.request_page.text
     
     def find_title(self, s):
